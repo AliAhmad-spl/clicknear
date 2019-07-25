1b1b3609203b408f83d4d7f17d5735e3e0f3f78a
aliahmad66 <ali.ahmed@buzzinteractive.co>
2019-07-25 23:35:51 +0500

    initial commit

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,12 +1,12 @@
-require 'carrierwave/orm/activerecord'
-require 'fog'
+# require 'carrierwave/orm/activerecord'
+# require 'fog'
 
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => "access key here",
-      :aws_secret_access_key  => "access secret key here",
-      :region                 => 'region here' # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = "directory here"
-end
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#       :provider               => 'AWS',
+#       :aws_access_key_id      => "access key here",
+#       :aws_secret_access_key  => "access secret key here",
+#       :region                 => 'region here' # Change this for different AWS region. Default is 'us-east-1'
+#   }
+#   config.fog_directory  = "directory here"
+# end
